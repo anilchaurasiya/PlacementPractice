1fb666013d8b4b5d2afbe30e6239100eb37048d1
anilchaurasiya <66078010+anilchaurasiya@users.noreply.github.com>
2021-02-04 18:51:22 +0530

    Delete ATOZ.java

--- ATOZ.java
+++
@@ -1,9 +0,0 @@
-//6. WAP to Display Uppercased A to Z
-class ATOZ{
-	public static void main(String args[]){
-		for(int i=65; i<=90 ;i++ ){
-		    char c = (char)i;
-		System.out.print(c+" ");
-		}
-	}
-}
